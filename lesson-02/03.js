beb445b0de340c16aae0021127467802c1b09c7d
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-01 00:36:54 +0300

    com11

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,17 +1,16 @@
-const score = 70;
-let grade;
+let score = 70 // тестовое значение, можно изменять
+let grade
 
-if (score >= 90 && score <= 100) {
-    grade = 'A';
-} else if (score >= 80 && score <= 89) {
-    grade = 'B';
-} else if (score >= 70 && score <= 79) {
-    grade = 'C';
+// ваш код
+if (score >= 0 && score <= 49) {
+  grade = 'F'
 } else if (score >= 50 && score <= 69) {
-    grade = 'D';
-} else if (score >= 0 && score <= 45) {
-    grade = 'F';
-} else {
-    grade = 'Invalid score';
+  grade = 'D'
+} else if (score >= 70 && score <= 79) {
+  grade = 'C'
+} else if (score >= 80 && score <= 89) {
+  grade = 'B'
+} else if (score >= 90 && score <= 100) {
+  grade = 'A'
 }
 console.log(grade);
