40dcfc402127f480faf0dcd0ee15015e49e7fdde
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-01 00:04:44 +0300

    com3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,18 +1,18 @@
-const score = 0
-let grade
+const score = 0; // This will be dynamically set in your tests
+let grade;
 
 if (score >= 90 && score <= 100) {
-  grade = 'A'
+  grade = 'A';
 } else if (score >= 80 && score <= 89) {
-  grade = 'B'
+  grade = 'B';
 } else if (score >= 70 && score <= 79) {
-  grade = 'C'
+  grade = 'C';
 } else if (score >= 46 && score <= 69) {
-  grade = 'D'
+  grade = 'D';
 } else if (score >= 0 && score <= 45) {
-  grade = 'F'
+  grade = 'F';
 } else {
-  grade = 'Invalid score'
+  grade = 'Invalid score';
 }
 
-console.log(grade)
+console.log(grade);
