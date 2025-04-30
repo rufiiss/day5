c31846c88d47cf09a676cc31898f9d50ea5fcf06
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-01 00:56:55 +0300

    c2

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,19 +1,24 @@
-const score = 70; // тестовое значение, можно изменять
+const score = 55; // тестовое значение, можно изменять
 let grade;
 
 // Определение оценки в зависимости от балла
 if (score >= 90 && score <= 100) {
-    grade == 'A';
+    grade = 'A';
 } else if (score >= 80 && score <= 89) {
-    grade == 'B';
+    grade = 'B';
 } else if (score >= 70 && score <= 79) {
-    grade == 'C';
+    grade = 'C';
 } else if (score >= 50 && score <= 69) {
-    grade == 'D';
+    grade = 'D';
 } else if (score >= 0 && score <= 49) {
-    grade == 'F';
+    grade = 'F';
 } else {
     grade = 'Invalid score'; // Если балл вне диапазона
 }
 
 console.log(grade); // Выводим результат
+const studentCodeWithoutDeclarations = (0, removeOriginalDeclarations_1.removeOriginalDeclarations)(this.studentCode, ['score']);
+const testCode = `let score = ${score}; ${studentCodeWithoutDeclarations}; return grade;`;
+const executeScript = new Function(testCode);
+const grade = executeScript();
+(0, local_cypress_1.expect)(grade).to.equal(expectedGrade);
