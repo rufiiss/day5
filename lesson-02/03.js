7aade8f0eef1412199e39da1f3efa98061d33f78
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-01 00:10:15 +0300

    com5

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,22 +1,18 @@
-const studentCodeWithoutDeclarations = `
+const score = 70;
 let grade;
+
 if (score >= 90 && score <= 100) {
     grade = 'A';
 } else if (score >= 80 && score <= 89) {
     grade = 'B';
 } else if (score >= 70 && score <= 79) {
     grade = 'C';
-} else if (score >= 46 && score <= 69) {
+} else if (score >= 50 && score <= 69) {
     grade = 'D';
-} else if (score >= 0 && score <= 45) {
+} else if (score >= 0 && score <= 49) {
     grade = 'F';
 } else {
     grade = 'Invalid score';
 }
-`;
 
-const testCode = `let score = ${score}; ${studentCodeWithoutDeclarations} return grade;`;
-const executeScript = new Function(testCode);
-const grade = executeScript();
-
-console.log(grade);
+console.log(grade); 
