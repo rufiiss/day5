2d7d4c8aacba13a49f7540b5f1dfa45a24904081
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-01 00:33:32 +0300

    com10

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -9,10 +9,9 @@ if (score >= 90 && score <= 100) {
     grade = 'C';
 } else if (score >= 50 && score <= 69) {
     grade = 'D';
-} else if (score >= 0 && score <= 49) {
+} else if (score >= 0 && score <= 45) {
     grade = 'F';
 } else {
     grade = 'Invalid score';
 }
-
 console.log(grade);
