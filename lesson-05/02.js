af69cb083703178cebcfcdae4f130ca3e9a2e3e0
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-15 19:00:45 +0300

    l5.2.1

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -2,7 +2,14 @@ function updateGallery(gallery, artwork, newValue) {
   gallery[artwork] = newValue; // Обновляем или добавляем произведение
 }
 
-// Примеры использования:
+// Объявляем объект галереи перед вызовами
+const gallery = {
+  'Mona Lisa': 'Leonardo da Vinci',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch',
+};
+
+// Вызываем функцию для обновления галереи
 updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
 updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
 
