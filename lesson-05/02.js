ebdea834bc095e1c283aed1da61034e53a476c66
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-05-15 18:58:02 +0300

    l5.2

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,38 +1,9 @@
-/*
-Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
-
-- объект галереи
-- название произведения (ключ)
-- новое значение
-
-Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
-
-Пример использования функции:
-
-const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch'
+function updateGallery(gallery, artwork, newValue) {
+  gallery[artwork] = newValue; // Обновляем или добавляем произведение
 }
 
-updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
-updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+// Примеры использования:
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
 
-console.log(gallery)
-
-Ожидаемый вывод:
-{
-  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-  'The Persistence of Memory': 'Salvador Dali'
-}
-*/
-
-const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-}
-
-function updateGallery() {}
+console.log(gallery);
